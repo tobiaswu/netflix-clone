25427f297cc878e15be6a32d68905bfa0cad5786
Tobias Wupperfeld <tobias@wupperfeld.org>
2020-08-10 10:25:18 +0200

    initial commit

---
+++ src/Nav.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll");
+    };
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav-logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+
+      <img className="nav_avatar" src="../netflix-avatar.jpg" alt="Avatar" />
+    </div>
+  );
+}
+
+export default Nav;
